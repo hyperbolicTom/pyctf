c7fd809cfa87fb4f02d53b39a28d342e9787ac3d
Tom Holroyd <tomh@kurage.nimh.nih.gov>
2019-01-07 14:43:41 -0500

    remove references to deleted things

--- pyctf/__init__.py
+++ pyctf/__init__.py
@@ -4,7 +4,6 @@ from .dsopen import dsopen, PRI_idx
 from . import ctf_res4 as ctf
 from . import fid, samiir, st, util
 from .samiir import *
-from .chl import CHLocalizer
 from .segments import get_segment_list, onlyTrials
 from .paramDict import paramDict
 # in case there's no display
